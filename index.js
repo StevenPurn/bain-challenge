d5cfa39db766ba852a4566d4b6e116c2224c8924
StevenPurn <smp5119@gmail.com>
2018-07-11 20:54:28 -0700

    Checking user input is valid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let data;
+const { types, states, sizes } = require('constants');
 const { readFile } = require('fs');
+let data;
 
 if (process.argv[2]) {
   readFile(process.argv[2], 'utf8', (err, fileContents) => {
@@ -10,9 +11,27 @@ if (process.argv[2]) {
       data = JSON.parse(fileContents);
       const search = process.argv[3];
       const value = process.argv[4];
-      console.log(data);
-      console.log(search);
-      console.log(value);
+      if (isInputValid(search, value)) {
+        console.log('Input is valid');
+      } else {
+        console.log('Invalid input, please check your syntax');
+      }
     }
   });
+}
+
+const isInputValid = (search, value) => {
+  switch(search) {
+    case 'find_after':
+    case 'find_before':
+      return !isNaN(value);
+    case 'find_type':
+      return types.includes(value);
+    case 'locate':
+      return states.includes(value);
+    case 'find_companies_between_size':
+      return sizes.includes(value);
+    default:
+      return false;
+  }
 }
